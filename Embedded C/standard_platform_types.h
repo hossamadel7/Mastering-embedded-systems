ac8f3ef961855e0cb4d35b00695113a4b9412ed9
Mohamed <mm.ahmedd504@gmail.com>
2023-07-04 11:51:21 +0300

    standard_platform_types

--- Embedded C/standard_platform_types.h
+++ Embedded C/standard_platform_types.h
@@ -3,6 +3,17 @@
 
 #include <stdint.h>
 
+#ifndef FALSE
+#define FALSE  (boolean)false
+#endif
+
+#ifndef TRUE
+#define TRUE  (boolean)true
+#endif
+
+
+typedef unsigned char boolean;
+typedef char char_t;
 
 typedef int8_t sint8;
 typedef uint8_t uint8;
@@ -20,6 +31,12 @@ typedef int32_t sint32;
 typedef uint32_t uint32;
 typedef volatile int32_t vint32;
 typedef volatile uint32_t vuint32;
+
+
+typedef int64_t sint64;
+typedef uint64_t uint64;
+typedef volatile int64_t vint64;
+typedef volatile uint64_t vuint64;
 
 
 #endif
