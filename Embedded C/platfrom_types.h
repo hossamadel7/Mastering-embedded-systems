1554db8a3993f6d7f4497ef3cd2de82f558ee4e5
Mohamed <mm.ahmedd504@gmail.com>
2023-07-04 11:46:00 +0300

    platform types

---
+++ Embedded C/platfrom_types.h
@@ -0,0 +1,27 @@
+#ifndef MY_PLATFORM_TYPES_H_
+#define MY_PLATFORM_TYPES_H_
+
+
+
+typedef unsigned char boolean;
+
+typedef unsigned char uint8_t;
+typedef char sint8_t;
+typedef volatile unsigned char vuint8_t;
+typedef volatile char vsint8_t;
+
+
+typedef unsigned short uint16_t;
+typedef short sint16_t;
+typedef volatile unsigned short vuint16_t;
+typedef volatile short vsint16_t;
+
+
+typedef unsigned int uint32_t;
+typedef int sint32_t;
+typedef volatile unsigned int vuint32_t;
+typedef volatile int vsint32_t;
+
+
+
+#endif
